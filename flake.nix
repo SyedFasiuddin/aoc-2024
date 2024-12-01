737fd82f5291950f63638898a8921424d7a019af
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2024-12-01 16:58:48 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "aoc-2024";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    in
+    {
+      packages."aarch64-darwin".default = pkgs.stdenv.mkDerivation {
+        name = "aoc";
+        src = ./.;
+
+        buildInputs = with pkgs; [
+          rustc cargo rustfmt
+        ];
+      };
+    };
+}
