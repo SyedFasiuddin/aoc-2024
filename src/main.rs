15dc3b62029c70f8192e5a1f30cba0a103db42aa
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2024-12-01 17:36:56 +0530

    refactor day1

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,16 @@ use std::io::Read;
 use std::collections::HashMap;
 
 #[allow(dead_code)]
-fn day1a(input: &str) {
-    let mut left: Vec<u32> = vec![];
-    let mut right: Vec<u32> = vec![];
-    for line in input.lines() {
-        let v = line
-            .split_whitespace()
-            .map(|x| x.parse().expect("Unable to convert str to u32"))
-            .collect::<Vec<u32>>();
-        left.push(v[0]);
-        right.push(v[1]);
-    }
-    left.sort();
-    right.sort();
+fn day1a(left: Vec<u32>, right: Vec<u32>) -> u32 {
     let mut distance = 0;
     for (l, r) in left.into_iter().zip(right) {
         distance += r.abs_diff(l);
     }
-    println!("distance: {}", distance);
+    distance
 }
 
-fn day1b(input: &str) {
-    let mut left: Vec<u32> = vec![];
-    let mut right: Vec<u32> = vec![];
-    for line in input.lines() {
-        let v = line
-            .split_whitespace()
-            .map(|x| x.parse().expect("Unable to convert str to u32"))
-            .collect::<Vec<u32>>();
-        left.push(v[0]);
-        right.push(v[1]);
-    }
-    left.sort();
-    right.sort();
-
+#[allow(dead_code)]
+fn day1b(left: Vec<u32>, right: Vec<u32>) -> u32 {
     let mut right_hash: HashMap<u32, u32> = Default::default();
     for r in right {
         right_hash.entry(r).and_modify(|entry| *entry += 1).or_insert(1);
@@ -48,12 +24,31 @@ fn day1b(input: &str) {
             similarity += l * val;
         }
     }
-    println!("similarity: {}", similarity);
+    similarity
+}
+
+#[allow(dead_code)]
+fn day1(input: &str) {
+    let mut left: Vec<u32> = vec![];
+    let mut right: Vec<u32> = vec![];
+    for line in input.lines() {
+        let v = line
+            .split_whitespace()
+            .map(|x| x.parse().expect("Unable to convert str to u32"))
+            .collect::<Vec<u32>>();
+        left.push(v[0]);
+        right.push(v[1]);
+    }
+    left.sort();
+    right.sort();
+
+    // println!("Part A, distance: {}", day1a(left, right));
+    println!("Part B, similarity: {}", day1b(left, right));
 }
 
 fn main() {
     let mut str = String::new();
     let _ = File::open("inputs/1.txt").unwrap().read_to_string(&mut str);
 
-    day1b(&str);
+    day1(&str);
 }
