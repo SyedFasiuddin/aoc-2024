34e25258d4c5015e9dcf1b2c974075a8030a2d4f
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2024-12-02 13:35:11 +0530

    day 2 another approach

--- src/day2.rs
+++ src/day2.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut str = String::new();
     let _ = File::open("inputs/2.txt").unwrap().read_to_string(&mut str);
 
-    day2b(&str);
+    day2a_another_approach(&str);
 }
 
 #[allow(dead_code)]
@@ -44,6 +44,7 @@ fn day2a(input: &str) {
     println!("safe: {:?}", num_safe);
 }
 
+#[allow(dead_code)]
 fn day2b(input: &str) {
     let mut num_safe = 0;
     'outer: for report in input.lines() {
@@ -66,7 +67,7 @@ fn day2b(input: &str) {
                         if !damped {
                             damped = true
                         } else {
-                            continue 'outer
+                            continue 'outer;
                         }
                     }
                 }
@@ -81,11 +82,38 @@ fn day2b(input: &str) {
                         if !damped {
                             damped = true
                         } else {
-                            continue 'outer
+                            continue 'outer;
                         }
                     }
                 }
             }
+            num_safe += 1;
+        }
+    }
+    println!("safe: {:?}", num_safe);
+}
+
+#[allow(dead_code)]
+fn day2a_another_approach(input: &str) {
+    let mut num_safe = 0;
+    for report in input.lines() {
+        let levels = report
+            .split_whitespace()
+            .map(|v| {
+                v.parse::<i32>()
+                    .expect("ERROR: unable to convert str to usize")
+            })
+            .collect::<Vec<_>>();
+
+        let diffs = levels
+            .windows(2)
+            .map(|window| window[1] - window[0])
+            .collect::<Vec<_>>();
+
+        // Can't use `.all` before doing `.collect`
+        if diffs.iter().all(|&val| val == -1 || val == -2 || val == -3) {
+            num_safe += 1;
+        } else if diffs.iter().all(|&val| val == 1 || val == 2 || val == 3) {
             num_safe += 1;
         }
     }
