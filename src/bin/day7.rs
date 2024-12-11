b47f7a64ce16e18475c58e440c524176557736e5
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2024-12-11 10:50:13 +0530

    day 7 part b

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -15,12 +15,13 @@ fn permutation<T: std::clone::Clone>(ret: &mut Vec<Vec<T>>, of: &mut [T], idx: u
     }
 }
 
-/// Generate permutations of arbitrary length of '+' and '*'
+/// Generate permutations of arbitrary length of '+', '*' and '|'
 fn permutation2(
     ret: &mut Vec<Vec<char>>,
     arr: &mut Vec<char>,
     curr_depth: usize,
     max_depth: usize,
+    partb: bool,
 ) {
     if curr_depth >= max_depth {
         ret.push(arr.to_vec());
@@ -28,16 +29,22 @@ fn permutation2(
     }
 
     arr.push('+');
-    permutation2(ret, arr, curr_depth + 1, max_depth);
+    permutation2(ret, arr, curr_depth + 1, max_depth, partb);
     arr.pop().unwrap();
 
     arr.push('*');
-    permutation2(ret, arr, curr_depth + 1, max_depth);
+    permutation2(ret, arr, curr_depth + 1, max_depth, partb);
     arr.pop().unwrap();
+
+    if partb {
+        arr.push('|');
+        permutation2(ret, arr, curr_depth + 1, max_depth, partb);
+        arr.pop().unwrap();
+    }
 }
 
 // https://github.com/maciekbartczak/aoc-2024/blob/main/src/bin/07.rs
-fn parta(input: &str) {
+fn solve(input: &str, partb: bool) {
     let input = input
         .lines()
         .map(|line| {
@@ -65,7 +72,7 @@ fn parta(input: &str) {
         let mut perms: Vec<Vec<char>> = Default::default();
         let mut arr: Vec<char> = Default::default();
 
-        permutation2(&mut perms, &mut arr, 0, values.len() - 1);
+        permutation2(&mut perms, &mut arr, 0, values.len() - 1, partb);
 
         let mut adds_up = false;
         for perm in perms {
@@ -78,6 +85,10 @@ fn parta(input: &str) {
                     running_total += v.pop().unwrap();
                 } else if ops == '*' {
                     running_total *= v.pop().unwrap();
+                } else if ops == '|' {
+                    let val = v.pop().unwrap();
+                    let val = format!("{}{}", running_total.to_string(), val.to_string());
+                    running_total = val.parse().unwrap();
                 }
             }
 
@@ -99,5 +110,5 @@ fn main() {
     let mut str = String::new();
     let _ = File::open("inputs/7.txt").unwrap().read_to_string(&mut str);
 
-    parta(&str);
+    solve(&str, true);
 }
